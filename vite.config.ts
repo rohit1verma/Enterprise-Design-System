1c3f7b901be68d0519cd170f776a81c5f63a6927
rohit1verma <vermarohit7780@gmail.com>
2025-04-22 23:41:22 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base:process.env.VITE_BASE_PATH || "/Enterprise-Design-System",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
